69cd5f63f433c982f2adf538b562bb7f2d790dcb
Olgica04 <82290208+Olgica04@users.noreply.github.com>
2025-03-19 22:19:47 +0100

    Add files via upload

---
+++ Homework-Class03/StudentGroup/Program.cs
@@ -0,0 +1,35 @@
+﻿string[] studentsG1 = new string[] { "Ivana", "Dejan", "Jovana", "Goran", "Bojan" };
+string[] studentsG2 = new string[] { "Kosta", "Marija", "Davor", "Eva", "Ena" };
+
+Console.WriteLine("Enter student group:");
+bool success = int.TryParse(Console.ReadLine(), out int number);
+
+if (success)
+{
+    if (number == 1)
+    {
+        Console.WriteLine("The students in G1 are: ");
+        foreach (string item in studentsG1)
+        {
+            Console.WriteLine(item);
+        }
+    }
+    else if (number == 2)
+    {
+        Console.WriteLine("The students in G2 are: ");
+        foreach (string item in studentsG2)
+        {
+            Console.WriteLine(item);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Wrong input! Please enter a number between 1 and 2");
+    }
+}
+else
+{
+    Console.WriteLine("Wrong input!");
+}
+
+
---
+++ Homework-Class03/SumOfEven/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] arrayOfNumbers = new int[6];
+
+for (int i = 0; i < arrayOfNumbers.Length; i++)
+{
+    Console.WriteLine("Enter integer no: " + (i + 1));
+    bool success = int.TryParse(Console.ReadLine(), out int number);
+
+
+    if (success)
+    {
+        arrayOfNumbers[i] = number;
+    }
+    else
+    {
+        Console.WriteLine("Wrong input! Please enter a valid input!");
+        break;
+    }
+
+}
+
+int sum = 0;
+for (int i=0; i < arrayOfNumbers.Length; i++)
+{
+    if (arrayOfNumbers[i] % 2 == 0)
+    {
+    sum += arrayOfNumbers[i];
+    }
+}
+Console.WriteLine("The resault is: " + sum);
+    
+
+
+
