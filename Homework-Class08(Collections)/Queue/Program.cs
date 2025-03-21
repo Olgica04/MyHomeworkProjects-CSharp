b4077c22c48247f4bedfe479fc80655fd1750107
Olgica04 <82290208+Olgica04@users.noreply.github.com>
2025-03-21 21:20:50 +0100

    Add files via upload

---
+++ Homework-Class08(Collections)/Queue/Program.cs
@@ -0,0 +1,32 @@
+﻿Queue<int> numbers = new Queue<int>();
+
+string input;
+
+do
+{
+    Console.WriteLine("Enter number: ");
+    input = Console.ReadLine();
+    bool success = int.TryParse(input, out int number);
+
+    if (success)
+    {
+        numbers.Enqueue(number);
+    }
+    else
+    {
+        Console.WriteLine("Please enter a valid number!");
+        continue;
+    }
+
+    Console.WriteLine("Do you want to enter another number? (YES/NO): ");
+
+    input = Console.ReadLine().ToUpper();
+
+
+} while (input == "YES");
+
+Console.WriteLine("Numbers in queue: ");
+foreach (int num in numbers)
+{
+    Console.WriteLine(num);
+}
